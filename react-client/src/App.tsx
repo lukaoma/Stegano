f039286e45a9c48d2a8abf2669856f4387b5eb97
Larry <lukaoma@utexas.edu>
2019-11-30 00:11:15 -0600

    lets try this

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -9,6 +9,8 @@ const App: React.FC = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          // @ts-ignore
+          <p>My Token = {window.token}</p>
         </p>
         <a
           className="App-link"
